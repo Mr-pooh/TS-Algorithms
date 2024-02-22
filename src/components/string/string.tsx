01fab63a34f0ab02f0a4f0c665356a5ee87726b0
Nikita Bokov <nekit123333@yandex.ru>
2024-02-22 14:43:43 +0300

    first commit

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
-
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import style from './string.module.css'
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+      <form className={style.form}>
+        <Input isLimitText={true} maxLength={11}></Input>
+        <Button text={`Развернуть`} ></Button>
+      </form>
     </SolutionLayout>
   );
 };
