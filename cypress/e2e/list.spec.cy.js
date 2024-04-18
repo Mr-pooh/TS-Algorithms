9e598cda171a56f6da825f0415c55e517a5ea3f3
pooh <nekit123333@yandex.ru>
2024-04-18 21:23:11 +0300

    закончить тесты для  list

--- cypress/e2e/list.spec.cy.js
+++ cypress/e2e/list.spec.cy.js
@@ -6,16 +6,16 @@ import {
   testCircleContain,
   testCircleHead,
   testCircleIndex,
+  testCircleLetter,
   testCircleTail,
   testCirlce,
   testUrl,
 } from "../../src/constants/testConstant";
 
 describe("List", () => {
-
-    const defaultColor = "rgb(0, 50, 255)";
-    const modifiedColor = "rgb(210, 82, 225)";
-    const changingColor = "rgb(127, 224, 81)";
+  const defaultColor = "rgb(0, 50, 255)";
+  const modifiedColor = "rgb(210, 82, 225)";
+  const changingColor = "rgb(127, 224, 81)";
 
   beforeEach(() => {
     cy.visit(`${testUrl}/list`);
@@ -29,32 +29,34 @@ describe("List", () => {
     cy.get(buttonAddIndexList).should("be.disabled");
     cy.get(buttonDeleteIndexList).should("be.disabled");
   });
-  it('correct render default list', ()=> {
-    cy.get(testCirlce).each(($el, index,$list)=> {
-        cy.get($list).should("have.length", 4)
-        cy.get($el).should("have.css", "border-color", defaultColor);
-    })
+  it("correct render default list", () => {
+    cy.get(testCirlce).each(($el, index, $list) => {
+      cy.get($list).should("have.length", 4);
+      cy.get($el).should("have.css", "border-color", defaultColor);
+    });
 
-    cy.get(testCircleIndex).each(($el, index, $list)=> {
-        cy.get($el).contains(index)
-    })
-    
-    cy.get(testCircleHead).each(($el, index, $list)=> {
-        if(index === 0){
-            cy.get($el).contains('head')
-        }
-        else {
-            cy.get($el).should('be.empty')
-        }
-    })
-    cy.get(testCircleTail).each(($el, index, $list)=> {
-        if(index !== $list.length-1){
-            cy.get($el).should('be.empty')
-        }
-        else {
-            cy.get($el).contains('tail')
-        }
-    })
+    cy.get(testCircleIndex).each(($el, index, $list) => {
+      cy.get($el).should("have.text", index);
+    });
 
-})
+    cy.get(testCircleHead).each(($el, index, $list) => {
+      if (index === 0) {
+        cy.get($el).should("have.text", "head");
+      } else {
+        cy.get($el).should("not.have.text", "head");
+      }
+    });
+    cy.get(testCircleTail).each(($el, index, $list) => {
+      if (index !== $list.length - 1) {
+        cy.get($el).should("not.have.text", "tail");
+      } else {
+        cy.get($el).should("have.text", "tail");
+      }
+    });
+
+    cy.get(testCircleLetter).each(($el, index) => {
+      cy.get($el).should("be.not.empty");
+    });
+  });
+  it("correct add element in head", () => {});
 });
